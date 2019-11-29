96388be3a199dcd8ed8364bff8e142ec9f304b12
Fedex <fedex6@gmail.com>
2019-11-29 14:53:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,23 @@ switch($_POST['acc']){
         break;
 
     default:
+        ?>
+        <form method="POST">
+            <label>Documento: </label><br />
+            <input type="text" name="documento" value="" />
+
+            <br /><br />
+            <label>Monto: (coma o punto para dividir los decimales)</label><br />
+            <input type="text" name="monto" value="" />
+
+            <br /><br />
+            <label>Token: </label><br />
+            <input type="text" name="token" value="" />
+
+            <br /><br />
+            <input type="submit" name="acc" value="Generar" />
+        </form>
+        <?php
         break;
 }
 
