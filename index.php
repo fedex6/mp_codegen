9b6fdcc35c106405d6d0108a971b276f6cc14262
Fedex <fedex6@gmail.com>
2019-12-02 11:15:02 -0300

    Updates
    Rediseño de la pagina.
    + fondo

--- index.php
+++ index.php
@@ -2,68 +2,109 @@
 <html>
 <head>
     <title>Generador de Codigos para MP->BruBank</title>
+    <link href="https://fonts.googleapis.com/css?family=Sulphur+Point&display=swap" rel="stylesheet">
     <style type="text/css">
-        @font-face {
-            font-family: "CODE39";
-            src: local("Code39FireRedA"), url("font/C39fira.TTF") format("truetype");
-        }
-            * { /*box-shadow: inset 0 0 0 1px red;*/ font-size: 20pt; font-family: 'Helvetica', 'Arial', sans-serif; }
-            body { text-align: center; padding: 0; margin: 0; width: 100vw; background: #FFF; }
-            input { width: 90vw; font-size: 30pt; }
-
-            .codigo { font-family: "CODE39"; font-size: 30px; }
+            * { /*box-shadow: inset 0 0 0 1px red;*/ font-size: 18pt; font-family: 'Sulphur Point', sans-serif; }
+            body { text-align: center; padding: 0; margin: 0; width: 100vw; background: url('background.png'); background-color: #262241; }
+            input { width: 50vw; font-size: 24pt; }
+            footer{ font-size: 8pt; margin-top: 15px; }
+            h2 { padding: 0; margin: 0; }
+            .container{
+                width: 60vw;
+                background: #FFF;
+                margin: 5px auto;
+                padding: 15px;
+                    /* Borde Redondeado */
+                    -webkit-border-radius: 10px;
+                    -moz-border-radius: 10px;
+                    border-radius: 10px;
+                    /* Sombra */
+                    -webkit-box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.3);
+                    -moz-box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.3);
+                    box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.3);
+            }
     </style>
 </head>
 
 <body>
-    <br /><br />
-    <h2>Generador de Codigo de Barras para pagos MP -> BruBank</h2>
-    <br />
-    <?php
-    switch($_POST['acc']){
-        case 'Generar':
-            $documento = str_pad($_POST['documento'], 8, "0", STR_PAD_LEFT);
-            $separadores = array(",", ".");
-            $valor = str_replace($separadores , '' , $_POST['monto']);
+    <img src="logo.png">
+    <div class="container">
+        
+        <h2>Generador de Codigo de Barras</h2>
+        <?php
+        switch($_POST['acc']){
+            case 'Generar':
+                $documento = str_pad($_POST['documento'], 8, "0", STR_PAD_LEFT);
+                $separadores = array(",", ".");
+                $valor = str_replace($separadores , '' , $_POST['monto']);
+                $monto = str_pad($valor, 10, 0, STR_PAD_LEFT);
+                switch ($_POST['entidad']) {
+                    case 'uala':
+                        ?>
+                        <h1>UALA</h1>
+                        <img src="https://barcode.tec-it.com/barcode.ashx?data=900620330000000<?php echo $documento; ?>000000O&code=&multiplebarcodes=false&translate-esc=false&unit=Fit&dpi=96&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&qunit=Mm&quiet=0"/><br /><br />
+                        <strong>900620330000000<?php echo $documento; ?>000000O</strong>
+                        <?php
+                        break;
 
-            $monto = str_pad($valor, 10, 0, STR_PAD_LEFT);
-            ?>
-
-            <!-- <span class="codigo">*9006220300<?php echo $documento.$monto.$_POST['token']; ?>00000O*</span>-->
-            <img src="https://barcode.tec-it.com/barcode.ashx?data=9006220300<?php echo $documento.$monto.$_POST['token']; ?>00000O&code=&multiplebarcodes=false&translate-esc=false&unit=Fit&dpi=96&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&qunit=Mm&quiet=0"/><br /><br />
-            <strong>9006220300<?php echo $documento.$monto.$_POST['token']; ?>00000O</strong>
-
-            <br /><br />
-            <a href="?volver=1" target="_self">Volver al Inicio</a>
-            <?php
-            break;
-
-        default:
-            ?>
-            <script type="text/javascript">
-                function setTwoNumberDecimal(el) {
-                    el.value = parseFloat(el.value).toFixed(2);
+                    case 'brubank':
+                        ?>
+                        <h1>BRUBANK</h1>
+                        <img src="https://barcode.tec-it.com/barcode.ashx?data=9006220300<?php echo $documento.$monto.$_POST['token']; ?>00000O&code=&multiplebarcodes=false&translate-esc=false&unit=Fit&dpi=96&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&qunit=Mm&quiet=0"/><br /><br />
+                        <strong>9006220300<?php echo $documento.$monto.$_POST['token']; ?>00000O</strong>
+                        <?php  
+                        break;
+                        
+                    case '':
+                        echo '<span style="color: red;">* No seleccionaste Entidad</span>';
+                        break;
                 }
-            </script>
-            <form method="POST">
-                <label>Documento: </label><br />
-                <input type="text" name="documento" value="" />
+                ?>
 
                 <br /><br />
-                <label>Monto: (coma o punto para dividir los decimales)</label><br />
-                <input type="number" name="monto" step="0.01" onchange="javascript:setTwoNumberDecimal(this);" />
+                <a href="?volver=1" target="_self">Volver al Inicio</a>
+                <?php
+                break;
+            default:
+                ?>
+                <script type="text/javascript">
+                    function setTwoNumberDecimal(el) {
+                        el.value = parseFloat(el.value).toFixed(2);
+                    }
+                </script>
+                <form method="POST">
+                    <label>Documento: </label><br />
+                    <input type="text" name="documento" value="" />
 
-                <br /><br />
-                <label>Token: </label><br />
-                <input type="text" name="token" value="" />
+                    <br />
+                    <label>Monto:</label><br />
+                    <input type="number" name="monto" step="0.01" onchange="javascript:setTwoNumberDecimal(this);" />
 
-                <br /><br />
-                <input type="submit" name="acc" value="Generar" />
-            </form>
-            <?php
-            break;
-    }
+                    <br />
+                    <label>Entidad: <span style="color: red;">*</span></label>
+                    <input type="radio" name="entidad" value="brubank" style="width: 26px !important; height: 30px;" /> BruBank | 
+                    <input type="radio" name="entidad" value="uala" style="width: 26px !important; height: 30px;"/> Ualá
 
-    ?>
+                    <br /><br />
+                    <label>Token: (Solo BruBank) </label><br />
+                    <input type="text" name="token" value="" />
+
+                    <br /><br />
+                    <input type="submit" name="acc" value="Generar" />
+                </form>
+                <?php
+                break;
+        }
+        ?>
+    </div>
+
+    <footer style="text-align: center;">
+        <a href="https://github.com/fedex6/mp_codegen" target="_new" style="text-decoration: none;"><img src="github.png" height="20px"></a>
+        <br />
+        <a href="https://cloudflare.com" target="_new" style="text-decoration: none;"><img src="https://www.cloudflare.com/media/images/web-badges/cf-web-badges-f-4.png" /></a>
+        <br />
+        <span style="color: #FFF; font-size: 8pt;">&copy; Poio <?php echo date('Y'); ?></span>
+    </footer>
+
 </body>
 </html>
